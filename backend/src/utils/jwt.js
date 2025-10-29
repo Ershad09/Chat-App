803abe0c986c8769845ea8ede7a665abdc23ee50
Ershad09 <ershad2809@gmail.com>
2025-10-29 20:57:02 +0530

    fix: resolve type error

--- backend/src/utils/jwt.js
+++ backend/src/utils/jwt.js
@@ -1,15 +1,15 @@
 import jwt from "jsonwebtoken";
 
 const sendToken = (userId, res) => {
-  const token = jwt.sign({ userId }, process.env.jwt_SECRET, {
+  const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
     expiresIn: "7d",
   });
 
   res.cookie("token", token, {
     httpOnly: true,
     sameSite: "strict",
-    secure: process.env.NODE_ENV === "devlopment" ? false : true,
-    maxAge:  7 * 24 * 60 * 60 * 1000
+    secure: process.env.NODE_ENV === "development" ? false : true,
+    maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
   });
 
   return token;
